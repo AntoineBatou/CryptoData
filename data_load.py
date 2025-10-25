6f3a452516a5144504d7bb592904fe9c65d0bd5a
Antoine Batou <toubas.antoine@gmail.com>
2025-10-25 20:30:07 +0200

    Ajout de la fonction de comparaison

--- data_load.py
+++ data_load.py
@@ -10,3 +10,18 @@ CREATE_TABLE = '''CREATE TABLE crypto_data (
 ADD_DATA = '''INSERT INTO crypto_data (name, price, market_cap, volume, time) VALUES (?, ?, ?, ?, ?);'''
 
 SHOW_DATA = '''SELECT * FROM crypto_data;'''
+
+SELECT_PREC_DATA = '''
+SELECT *
+FROM crypto_data
+WHERE name = ?
+ORDER BY time DESC
+LIMIT 2;'''
+
+SELECT_PREC_PRICE = '''
+SELECT price
+FROM crypto_data
+WHERE name = ?
+ORDER BY time DESC
+LIMIT 2;
+'''
--- main.py
+++ main.py
@@ -19,9 +19,9 @@ Faites simplement entré quand vous avez fait votre choix :
 2. Ethereum
 3. Solana '''
 
-CRYPTO_DICO = {"1": "bitcoin",
-               "2": "ethereum",
-               "3": "solana"}
+CRYPTO_DICO = {1: "bitcoin",
+               2: "ethereum",
+               3: "solana"}
 
 CRYPTO_CHOICE = [1, 2, 3]
 crypto_basket = []
@@ -61,23 +61,57 @@ def basket_choice():
     print(MENU_BASKET)
     while new_crypto := (input(MENU_BASKET)):
         if int(new_crypto) in CRYPTO_CHOICE:
-            crypto_basket.append(int(new_crypto))
+            if int(new_crypto) not in crypto_basket:
+                crypto_basket.append(int(new_crypto))
+                print(f"{CRYPTO_DICO.get(int(new_crypto))} ajouté !")
+            else:
+                print("Déjà dans le panier !")
         else:
             print("Mauvais choix !")
-    print(f"Votre choix : {crypto_basket}")
-    return crypto_basket
+    print("Votre choix :")
+    for i in crypto_basket:
+        print(f"- {CRYPTO_DICO.get(i).title()}")
+    liste = [CRYPTO_DICO[i] for i in crypto_basket]
+    return liste
+
+
+def get_prec(name):
+    with connex:
+        cursor = connex.cursor()
+        cursor.execute(data_load.SELECT_PREC_PRICE, (name,))
+        final = cursor.fetchall()
+        prix1, prix2 = final
+        final_tup = (prix1[0], prix2[0])
+        return final_tup
+
+def analyze_prec(tup):
+        prix1, prix2 = tup
+        print(tup)
+        diff = ((prix2 - prix1) / prix1) * 100
+        print(f"Différence de {diff} %")
+        return diff
+## En fait ici cette méthode ça ne va pas, il faut faire une requête pour récupérer le prix et le prix n_1 et calculer une diff
 
 while (user_choice := int(input(MENU))) != 6:
     create_base()
     if user_choice == 1:
         basket_choice()
     elif user_choice == 2:
-        ### A remplacer par un try except pour savoir si crypto basket existe
-        ### Simplifier la procédure ici pour basket_choice retourne directement la liste avec les str
-        liste = [CRYPTO_DICO[str(i)] for i in crypto_basket]
+        # Am&liorer cette répétition
+        liste = [CRYPTO_DICO[i] for i in crypto_basket]
+        if len(liste) == 0:
+            print("Vous n'avez pas défini de panier, donc on récupère les 100 premières !")
         get_and_save_data('usd', *liste)
     elif user_choice == 3:
-        pass
+        # Améliorer cette répétition
+        liste = [CRYPTO_DICO[i] for i in crypto_basket]
+        print(liste)
+        for i in liste:
+            print(i.title())
+            print("---")
+            valeurs = get_prec(i.title())
+            analyze_prec(valeurs)
+
     elif user_choice == 4:
         pass
     elif user_choice == 5:
@@ -85,4 +119,6 @@ while (user_choice := int(input(MENU))) != 6:
     else:
         print("Mauvais choix !")
 
-
+# if __name__ == "__main__":
+#     test = get_prec('Bitcoin')
+#     print(test)
